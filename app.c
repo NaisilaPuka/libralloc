5ceeaa758f0156d73cdef58c2f5c3b4176247336
Ibrahim Korpeoglu <korpe@cs.bilkent.edu.tr>
2019-04-10 21:41:57 +0300

    modified pid part

--- app.c
+++ app.c
@@ -16,27 +16,33 @@ pthread_t tids[N];            // ids of created threads
 
 void *aprocess (void *p)
 {
-    int pid = *((int*)p); 
     int req[3];
-    int k; 
-
+    int k;
+    int pid;
+    
+    pid =  *((int *)p);
+    
+    printf ("this is thread %d\n", pid);
+    fflush (stdout);
+    
     req[0] = 2;
     req[1] = 2;
     req[2] = 2;
     ralloc_maxdemand(pid, req); 
     
     for (k = 0; k < 10; ++k) {
-	req[0] = 1;
-	req[1] = 1;
-	req[2] = 1;
-	ralloc_request (pid, req);
+        
+        req[0] = 1;
+        req[1] = 1;
+        req[2] = 1;
+        ralloc_request (pid, req);
 	
-	// do something with the resources
+        // do something with the resources
 	
-	ralloc_release (pid, req);
+        ralloc_release (pid, req);
 
-	// call request and release as many times as you wish with
-	// different parameters
+        // call request and release as many times as you wish with
+        // different parameters
     }
     pthread_exit(NULL); 
 }
@@ -48,19 +54,25 @@ int main(int argc, char **argv)
     int deadlocked[N]; // array indicating deadlocked processes
     int k;
     int i;
-    int pid; 
+    int pids[N];
     
     for (k = 0; k < N; ++k)
-	deadlocked[k] = -1; // initialize    
+        deadlocked[k] = -1; // initialize
     
     handling_method = DEADLOCK_DETECTION;
     ralloc_init (N, M, exist, handling_method);
+
+    printf ("library initialized\n");
+    fflush(stdout);
     
     for (i = 0; i < N; ++i) {
-        pid = i;
+        pids[i] = i;
         pthread_create (&(tids[i]), NULL, (void *) &aprocess,
-                        (void *)&pid);
+                        (void *)&(pids[i])); 
     }
+    
+    printf ("threads created = %d\n", N);
+    fflush (stdout);
     
     while (1) {
         sleep (10); // detection period
